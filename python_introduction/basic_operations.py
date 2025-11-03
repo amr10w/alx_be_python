9c1722708fda52738f6695b31431e20bbccd3aae
Amr Wahidi <amrw5189@gmail.com>
2025-11-04 00:03:27 +0200

    fix

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,10 @@
 number1 =10
 number2 =5
 
-print(f'Addition of {number1} and {number2} is {number1+number2}')
-print(f'Subtraction  of {number1} and {number2} is {number2-number1}')
-print(f'Multiplication of {number1} and {number2} is {number1*number2}')
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {addition}')
+print(f'Subtraction  of {number1} and {number2} is {subtraction}')
+print(f'Multiplication of {number1} and {number2} is {multiplication}')
