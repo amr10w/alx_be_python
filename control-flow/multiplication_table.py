f4888ac24a5a3c89a0d08851c30d44381ca4d95d
Amr Wahidi <amrw5189@gmail.com>
2025-11-14 14:38:00 +0200

    fix error in mul

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(10):
-    print(f"{i+1} * {number} = {(i+1)*number}")
+    print(f"{number} * {i+1} = {(i+1)*number}")
